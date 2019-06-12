ec2309397b5557af86ef692522228167ece6d3af
Thomas Arts <thomas.arts@quviq.com>
2019-06-12 19:32:06 +0200

    update measure with contract call

--- test/measure_gas_SUITE.erl
+++ test/measure_gas_SUITE.erl
@@ -22,12 +22,37 @@ create_identy_contract(_Cfg) ->
     io:format("Fate: ~p\n", [Fate]),
 
     {Trees, Env} = init(),
-    {ok, Trees1, Env1} = tx_timer(100, aevm, Trees, Env, fun(T,E) -> contract_create(T, E, account(1), Aevm, <<"init">>, [], aevm) end),
-    {ok, Trees2, Env2} = tx_timer(100, fate, Trees1, Env1,  fun(T,E) -> contract_create(T, E, account(2), Fate, <<"init">>, [], fate) end),
+
+    ct:log("contract create"),
+    {ok, Trees1, Env1} = tx_timer(100, aevm, Trees, Env, fun(T,E) -> contract_create(T, E, account(1), Aevm, "init", [], aevm) end),
+    {ok, Trees2, _} = tx_timer(100, fate, Trees1, Env1,  fun(T,E) -> contract_create(T, E, account(2), Fate, "init", [], fate) end),
     {Trees3, Env3} = mine(1, Trees2),
 
-    %% {ok, Trees4, Env4} = tx_timer(100, aevm, Trees3, Env3, fun(T,E) -> contract_call(T, E, account(1), Aevm, <<"main">>, [42], aevm) end),
-    %% {ok, Trees5, Env5} = tx_timer(100, fate,vTrees4, Env4,  fun(T,E) -> contract_call(T, E, account(2), Fate, <<"main">>, [42], fate) end),
+    ct:log("Account ~p now ~p (total cost: ~p)", [1, balance(Trees3, 1), balance(Trees, 1) - balance(Trees3, 1)]),
+    ct:log("Account ~p now ~p (total cost: ~p)", [2, balance(Trees3, 2), balance(Trees, 2) - balance(Trees3, 2)]),
+    ct:log("Fate cheaper than AEVM: ~p per create", [(balance(Trees3, 2) - balance(Trees3, 1)) div 100]),
+
+    {ok, Trees4, Env4} = tx_timer(100, aevm, Trees3, Env3,
+                                  fun(T,E) -> contract_call(T, E, account(1),
+                                                            aect_contracts:compute_contract_pubkey(account(1), 1), Aevm, "main", ["42"], aevm) end),
+    {ok, Trees5, Env5} = tx_timer(100, fate, Trees4, Env4,
+                                  fun(T,E) -> contract_call(T, E, account(2),
+                                                            aect_contracts:compute_contract_pubkey(account(2), 1), Fate, "main", ["42"], fate) end),
+
+    ct:log("Account ~p now ~p (total cost: ~p)", [1, balance(Trees5, 1), balance(Trees, 1) - balance(Trees5, 1)]),
+    ct:log("Account ~p now ~p (total cost: ~p)", [2, balance(Trees5, 2), balance(Trees, 2) - balance(Trees5, 2)]),
+    ct:log("Fate cheaper than AEVM: ~p per call", [((balance(Trees3, 2) - balance(Trees5, 2)) -
+                                                      (balance(Trees3, 1) - balance(Trees5, 1))) div 100]),
+
+    AeVMCalls = aect_call_state_tree:to_list(aec_trees:calls(Trees4)),
+    FateCalls =  aect_call_state_tree:to_list(aec_trees:calls(Trees5)) -- AeVMCalls,
+
+    ct:log("Trees for aevm calls ~p\n", [lists:usort([{aect_call:gas_used(Call),
+                                                  aect_call:return_value(Call)} || {_, Call}<-AeVMCalls])]),
+    ct:log("Trees for fate calls ~p\n", [lists:usort([{aect_call:gas_used(Call),
+                                                       aect_call:return_value(Call)} || {_, Call}<-FateCalls])]),
+
+
     ok.
 
 
@@ -43,7 +68,7 @@ compile(File) ->
     {Aevm, Fate}.
 
 init() ->
-    init([{account, account(N), 200000000000000} || N<-lists:seq(1,9)]).
+    init([{account, account(N), 20000000000000000} || N<-lists:seq(1,9)]).
 
 init(Accounts) ->
     mine(0, trees_with_accounts(Accounts)).
@@ -73,9 +98,26 @@ contract_create(Trees, Env, Sender, CompiledContract, Init, Args, Backend) ->
     {ok, AeTx} = aect_create_tx:new(Tx),
     AeTx.
 
+contract_call(Trees, Env, Sender, ContractId, CompiledContract, Fun, Args, Backend) ->
+    #{byte_code := Code, contract_source := Contract} = CompiledContract,
+    {ok, CallData} = encode_call_data(Contract, Fun, Args, Backend),
+    Tx =
+        #{caller_id => aeser_id:create(account, Sender),
+          contract_id => aeser_id:create(contract, ContractId),
+          abi_version => case Backend of aevm -> 1; fate -> 3 end,
+          fee =>  500000 * 1000000,
+          gas_price => 1000000,
+          gas => 20000000,
+          nonce => nonce(Trees, Sender),
+          amount => 0,
+          call_data => CallData
+         },
+    {ok, AeTx} = aect_call_tx:new(Tx),
+    AeTx.
+
 
 encode_call_data(Code, Fun, Args, Backend) ->
-    case aeso_compiler:create_calldata(Code, binary_to_list(Fun), Args, [{backend, Backend}]) of
+    case aeso_compiler:create_calldata(Code, Fun, Args, [{backend, Backend}]) of
         {error, _} = Err -> Err;
         {ok, Data, _DataType, _OutType} when Backend == aevm ->
             {ok, Data};
@@ -100,6 +142,10 @@ account(N) ->
 nonce(Trees, Sender) ->
     {value, Account} = aec_accounts_trees:lookup(Sender, aec_trees:accounts(Trees)),
     aec_accounts:nonce(Account) + 1.
+
+balance(Trees, N) ->
+    {value, Account} = aec_accounts_trees:lookup(account(N), aec_trees:accounts(Trees)),
+    aec_accounts:balance(Account).
 
 trees_with_accounts(Accounts) ->
     trees_with_accounts(Accounts, aec_trees:new_without_backend()).
